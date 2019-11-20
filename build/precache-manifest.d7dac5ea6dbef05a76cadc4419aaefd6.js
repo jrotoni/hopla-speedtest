4ffa296b84fac171cd2be2bb5f667da28a1bc341
Jeremy Rotoni <jeremy.rotoni@gmail.com>
2019-11-20 17:45:18 +0800

    Add production build

---
+++ build/precache-manifest.d7dac5ea6dbef05a76cadc4419aaefd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383c589f7389c457bf0d8a5c718b675c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771ea8a8c07a27c1b9f0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1a97eb2ee81da6a6f6e9",
+    "url": "/static/js/2.2710fa94.chunk.js"
+  },
+  {
+    "revision": "771ea8a8c07a27c1b9f0",
+    "url": "/static/js/main.5b9f4bf8.chunk.js"
+  },
+  {
+    "revision": "d1fb6c07607d6f042650",
+    "url": "/static/js/runtime-main.1be62996.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
