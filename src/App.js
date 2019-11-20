26e37d76c623eca0fcc9bc1fa9e601a94e44ae14
Jeremy Rotoni <jeremy.rotoni@gmail.com>
2019-11-20 17:54:12 +0800

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Currently building Hopla Speed Measuring Gauge
+          Currently building Hopla Speed Measuring Gauge!
         </p>
         <a
           className="App-link"
